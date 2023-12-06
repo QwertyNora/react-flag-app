4b68717c412ca28b0c14c03933066e9a2608bbee
Nora Silfver <98233189+QwertyNora@users.noreply.github.com>
2023-12-06 17:08:21 +0100

    Changes

--- src/CountryInfo.jsx
+++ src/CountryInfo.jsx
@@ -1,19 +1,15 @@
 import React, { useState } from "react";
 
 function CountryInfo({ countryData }) {
- 
-
-  
   return (
     <>
       <div className="modal">
         <div className="modal-content">
           <p>Name: {countryData.name.common}</p>
-        <p>Official name: {countryData.name.official}</p>
-        <p>Capital: {countryData.capital}</p>
-        <p>Population: {countryData.population}</p>
+          <p>Official name: {countryData.name.official}</p>
+          <p>Capital: {countryData.capital}</p>
+          <p>Population: {countryData.population}</p>
         </div>
-        
       </div>
     </>
   );
